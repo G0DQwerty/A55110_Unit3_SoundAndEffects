e5601a3cbcf7c092f02bdf9e4fda56c05fbe9e37
David Figueiredo <150687060+G0DQwerty@users.noreply.github.com>
2024-03-13 18:38:50 +0000

    Unit3_Part3.1_End
    Some of the steps required code to be rewriten, so the code the code before being rewriten is added in the script as commets with the specific step on top of them

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     private Rigidbody playerRb;
-    //Step 3.3
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    //Step 3.1.3
     //void Start()
     //{
     //    playerRb = GetComponent<Rigidbody>();
@@ -15,14 +18,33 @@ public class PlayerControler : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        //Step 3.1.4
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    playerRb.AddForce(Vector3.up * 100 , ForceMode.Impulse);
+        //}
+
+        //Step 3.1.5
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        //}
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
-            playerRb.AddForce(Vector3.up * 100 , ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter (Collision collision)
+    {
+        isOnGround = true;
     }
 }
